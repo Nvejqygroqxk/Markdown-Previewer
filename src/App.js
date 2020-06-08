8aa1595f086b63ad645e48f14fc1ddb0b4aba074
Nvejqygroqxk <44037680+Nvejqygroqxk@users.noreply.github.com>
2020-06-08 05:13:27 +0200

    preview 100%, tests 6/7 no bonus

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ class TextArea extends React.Component {
 	render() {
 		return (
       <>
-			<div dangerouslySetInnerHTML={{__html: this.props.value}} />
-      <div>{this.props.value}</div>
+			<div id="preview" dangerouslySetInnerHTML={{__html: this.props.value}} />
       </>
 		);
 	}
@@ -16,7 +15,7 @@ class TextArea extends React.Component {
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: "One  \nTwo  \nThree  \nFour"};
+    this.state = {value: "One\n==\nTwo\n--\n[Three]\n\n`Four`\n\n[Three]: https://github.com/markedjs/marked/\n```\nFive\n```\n* Six\n* Seven\n>Eight\n>Nine\n\n![Google Logo](http://www.google.com/images/errors/logo_sm.gif)\n\n**Ten**"};
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -26,13 +25,11 @@ class NameForm extends React.Component {
 
   render() {
     return (
-      <>
       <div>
 				<textarea id="editor" onChange={this.handleChange} defaultValue={this.state.value}>
         </textarea>
-				<TextArea id="preview" value={marked(this.state.value,{sanitize: true})} />
-			</div>
-      </>
+          <TextArea value={marked(this.state.value,{sanitize: true})} />
+      </div>
     );
   }
 }
